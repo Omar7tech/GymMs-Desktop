91625a027dff331fff83d23a84d506202a1b71f9
Omar Abi Farraj <omar.7tech@gmail.com>
2026-02-20 14:52:38 +0200

    first commit

---
+++ app/Providers/NativeAppServiceProvider.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Providers;
+
+use Native\Desktop\Facades\Menu;
+use Native\Desktop\Facades\MenuBar;
+use Native\Desktop\Facades\Window;
+use Native\Desktop\Contracts\ProvidesPhpIni;
+
+class NativeAppServiceProvider implements ProvidesPhpIni
+{
+    /**
+     * Executed once the native application has been booted.
+     * Use this method to open windows, register global shortcuts, etc.
+     */
+    public function boot(): void
+    {
+        Window::open('dashboard')->route('dashboard')->maximized()->title('Gym Management System')->backgroundColor('#00000050')->hideMenu();
+        MenuBar::create()
+            ->withContextMenu(
+                Menu::make(
+                    Menu::label('My Application'),
+                    Menu::separator(),
+                    Menu::link('https://nativephp.com', 'Learn more…')
+                        ->openInBrowser(),
+                    Menu::separator(),
+                    Menu::quit()
+                )
+            );
+
+
+    }
+
+    /**
+     * Return an array of php.ini directives to be set.
+     */
+    public function phpIni(): array
+    {
+        return [
+            'memory_limit' => '512M',
+            'display_errors' => '1',
+            'error_reporting' => 'E_ALL',
+            'max_execution_time' => '0',
+            'max_input_time' => '0',
+        ];
+    }
+}
